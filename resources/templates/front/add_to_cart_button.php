cd370a01b3e5df12e7aae6c5be61195f7338330f
Max-F <max_fullmer@my.uri.edu>
2019-10-23 09:46:45 -0400

    minor cleaning

--- resources/templates/front/add_to_cart_button.php
+++ resources/templates/front/add_to_cart_button.php
@@ -1,1 +1,5 @@
-<button class="btn btn-primary">Add To Cart</button>
+<form action="">
+        <div class="form-group">
+            <input type="submit" class="btn btn-primary" value="ADD TO CART">
+        </div>
+</form>
