c871dc08a6e66b221f74c521e8e70f2258d2b757
Max-F <max_fullmer@my.uri.edu>
2019-11-07 22:24:14 -0500

    login redirects depending on if user is admin

---
+++ public/customer_orders.php
@@ -0,0 +1,24 @@
+<?php require_once("../resources/config.php"); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include(TEMPLATE_FRONT . DS . "head.php"); ?>
+
+<body>
+
+    <?php include(TEMPLATE_FRONT . DS . "top_nav.php"); ?>
+
+
+
+    <?php include(TEMPLATE_FRONT . DS . "footer.php"); ?>
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
