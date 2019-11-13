af74942ebe21cefd9b0be8f816c11bdf13986b54
Max-F <max_fullmer@my.uri.edu>
2019-11-13 14:29:34 -0500

    fixed product tabs spacing

--- resources/templates/front/head.php
+++ resources/templates/front/head.php
@@ -3,15 +3,15 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="description" content="SeedCommerce by Max Fullmer">
+    <meta name="author" content="Max Fullmer">
 
     <title>SeedCommerce</title>
 
     <link href="css/bootstrap.min.css" rel="stylesheet">
-
+    <link href="css/heroic-features.css" rel="stylesheet">
     <link href="css/shop-homepage.css" rel="stylesheet">
-    
+    <link href="css/shop-item.css" rel="stylesheet">
     <link href="css/styles.css" rel="stylesheet">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
