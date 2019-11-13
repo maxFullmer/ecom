23a15a0cef0632ce1d22a81ee619c0deebafde2c
Max-F <max_fullmer@my.uri.edu>
2019-11-13 11:43:38 -0500

    cleaned up comments

--- resources/templates/front/top_nav.php
+++ resources/templates/front/top_nav.php
@@ -1,35 +1,31 @@
-    <!-- Navigation -->
-    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-        <div class="container">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="index.php">SeedCommerce</a>
-            </div>
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav">
-                    <li>
-                        <a href="shop.php">Search the Catalog</a>
-                    </li>
-                    <li>
-                        <a href="checkout.php">Checkout Cart `\__]</a>
-                    </li>
-                    <li>
-                        <a href="login.php">Manage Product Orders</a>
-                    </li>                    
-                    <li>
-                        <a href="contact.php">Contact Us!</a>
-                    </li>
-
-                </ul>
-            </div>
-            <!-- /.navbar-collapse -->
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="index.php">SeedCommerce</a>
         </div>
-        <!-- /.container -->
-    </nav>
+
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li>
+                    <a href="shop.php">Search the Catalog</a>
+                </li>
+                <li>
+                    <a href="checkout.php">Checkout Cart `\__]</a>
+                </li>
+                <li>
+                    <a href="login.php">Manage Product Orders</a>
+                </li>                    
+                <li>
+                    <a href="contact.php">Contact Us!</a>
+                </li>
+
+            </ul>
+        </div>
+    </div>
+</nav>
