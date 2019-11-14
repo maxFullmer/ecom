d19fb5593ff491acce2125c2e453cfbca3186885
Max-F <max_fullmer@my.uri.edu>
2019-11-14 14:32:37 -0500

    links to other admin pages corrected

--- resources/templates/back/index_content_admin.php
+++ resources/templates/back/index_content_admin.php
@@ -16,7 +16,7 @@
                     </div>
                 </div>
             </div>
-            <a href="#">
+            <a href="./index.php?orders">
                 <div class="panel-footer">
                     <span class="pull-left">View Details</span>
                     <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
@@ -40,7 +40,7 @@
                     </div>
                 </div>
             </div>
-            <a href="#">
+            <a href="./index.php?products">
                 <div class="panel-footer">
                     <span class="pull-left">View Details</span>
                     <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
@@ -63,7 +63,7 @@
                     </div>
                 </div>
             </div>
-            <a href="#">
+            <a href="./index.php?categories">
                 <div class="panel-footer">
                     <span class="pull-left">View Details</span>
                     <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
