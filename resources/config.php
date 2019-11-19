c6d4e4d9ab4865e4455901598e51b3552ce5a17d
Max-F <max_fullmer@my.uri.edu>
2019-11-19 17:35:37 -0500

    updated comments

--- resources/config.php
+++ resources/config.php
@@ -17,5 +17,6 @@ defined("DB_NAME") ? null : define("DB_NAME", "ecom_db");
 
 $connection = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
 
+// store functionality
 require_once("store_functions.php");
 ?>
